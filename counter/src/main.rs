1ceeaef3da959581ca859012354151f58f002cc3
Jasper Schulz <jasper.b.schulz@gmail.com>
2016-09-26 14:49:44 +0100

    Updated example (to be a bit more complex).

--- counter/src/main.rs
+++ counter/src/main.rs
@@ -7,11 +7,12 @@ mod this;
 
 const DEC: usize = 0;
 const REP: usize = 1;
+const INC: usize = 2;
 
 fn main() {
     let mut my_driver = driver::Driver::default();
 
-    let program = [DEC, REP];
+    let program = [DEC, REP, INC, INC, INC, INC, INC, DEC, REP, INC];
 
     let mut cell = 10;
     let mut pc = 0;
@@ -19,7 +20,7 @@ fn main() {
     loop {
         pc = my_driver.merge_point(this::PROGRAM, this::IDX, &program, pc, &mut cell);
 
-        if pc >= 2 {
+        if pc >= 10 {
             break;
         }
 
@@ -27,12 +28,16 @@ fn main() {
 
         if opcode == DEC {
             cell -= 1;
+            println!("DEC");
         } else if opcode == REP && cell > 0 {
             pc -= 1;
             continue;
+        } else if opcode == INC {
+            cell += 1;
+            println!("INC");
         }
         pc += 1;
     }
 
-    println!("{:?}", cell);
+    // println!("{:?}", cell);
 }
